27d357f942f2795b03e50f00df4bdfbd51821541
watergun <jiashui@hotmail.com>
2017-04-22 03:50:32 -0500

    Add files via upload

--- MultiplyTable.java
+++ MultiplyTable.java
@@ -1,20 +1,12 @@
 /**
  * Created by jiash on 2017-04-22.
  */
-class OneMulty{
-    int m;
-    void multi(int n){
-        System.out.print(m + "*" + n + "=" + m*n + " ");
-    }
-}
 
 public class MultiplyTable {
     public static void main(String[] args){
-        OneMulty mul = new OneMulty();
-        for(int i=1;i<=9;i++){
-            mul.m=i;
-            for(int n=1;n<=i;n++)
-                mul.multi(n);
+        for(int m=1;m<=9;m++){
+            for(int n=1;n<=m;n++)
+                System.out.print(m + "*" + n + "=" + m*n + " ");
             System.out.println();
         }
     }
